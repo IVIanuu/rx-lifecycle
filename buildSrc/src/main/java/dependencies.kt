52409cf2dbac539adaf6a359ed35d1d24c6dc482
IVIanuu <IVIanuu@gmail.com>
2018-09-21 01:58:14 +0200

    Updated kotlin

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val androidGradlePlugin = "3.3.0-alpha10"
     const val androidx = "1.0.0-rc02"
     const val androidxArch = "2.0.0-rc01"
-    const val kotlin = "1.3-M2"
+    const val kotlin = "1.3.0-rc-57"
     const val mavenGradle = "2.1"
     const val rxAndroid = "2.1.0"
     const val rxJava = "2.2.1"
